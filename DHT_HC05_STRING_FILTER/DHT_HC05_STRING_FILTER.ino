8cb073cfdcbf662a7a450afdb8da515fbe6e9c14
Banini Prince <33152428+NerdBishop@users.noreply.github.com>
2019-02-15 15:17:56 +0000

    Update DHT_HC05_STRING_FILTER.ino

--- DHT_HC05_STRING_FILTER/DHT_HC05_STRING_FILTER.ino
+++ DHT_HC05_STRING_FILTER/DHT_HC05_STRING_FILTER.ino
@@ -1,21 +1,21 @@
 #include <DHT.h>
-#define DHTPIN 2
-#define DHTTYPE DHT22
+#define DHTPIN 2 //Digital Pin 2
+#define DHTTYPE DHT22 //DHT11 If you have a DH11
 DHT dht(DHTPIN, DHTTYPE);
 
-const int led_pin = 13;
+const int led_pin = 13; //LED Pin
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  dht.begin();
-  pinMode(led_pin, OUTPUT);
+  Serial.begin(9600); //Starting Serial Communication
+  dht.begin(); //Initiallising DHT Sensor
+  pinMode(led_pin, OUTPUT); //
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- int temp = dht.readTemperature();
+ int temp = dht.readTemperature(); 
  int hum = dht.readHumidity();
 
   Serial.print("Temp: ");
@@ -37,9 +37,9 @@ void loop() {
       c = c.substring(c.indexOf("#") + 1,c.lastIndexOf("#"));
       Serial.println(c);
       
-      if(c.equals("creed")){
+      if(c.equals("on")){
         digitalWrite(led_pin, HIGH);
-      }else if(c.equals("crood")){
+      }else if(c.equals("off")){
         digitalWrite(led_pin, LOW);
       }
     }
