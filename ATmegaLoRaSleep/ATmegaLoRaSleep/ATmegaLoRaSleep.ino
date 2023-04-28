17682eb7d2ad9884a1e4d38827937af9d649b11e
Banini Prince <princebanini007@gmail.com>
2023-04-28 08:16:02 +0000

    Putting Atmega and Lora to Sleep

--- ATmegaLoRaSleep/ATmegaLoRaSleep/ATmegaLoRaSleep.ino
+++ ATmegaLoRaSleep/ATmegaLoRaSleep/ATmegaLoRaSleep.ino
@@ -2,19 +2,34 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+const int mosfet1 = 6;
+const int mosfet2 = 7;
+const int onboard_LED = 8
+
 void setup() {
-  
+    pinMode(mosfet1, OUTPUT);
+    pinMode(mosfet2, OUTPUT);
+    pinMode(onboard_LED, OUTPUT);
+
+    delay(500);
+
+    digitalWrite(mosfet1, LOW);
+    digitalWrite(mosfet2, LOW);
+    digitalWrite(onboard_LED, LOW);
+
+    delay(500);
+    
    if (!LoRa.begin(868E6)) {
      Serial.println("LoRa init failed. Check your connections.");
      while (true);
    }
    
-   delay(1000);
+   delay(500);
 
    //Lora Sleep
    LoRa.sleep();
 
-   delay(1000);
+   delay(500);
    //ATmega Sleep
    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
